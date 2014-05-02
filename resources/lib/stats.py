07bec707a460bbbe1e43106d7f785cfc6478a551
Beam <ondra@bimovi.cz>
2014-05-02 13:50:59 +0200

    new stats url

--- resources/lib/stats.py
+++ resources/lib/stats.py
@@ -18,7 +18,7 @@ def results_with_stats(results, addon, title, item):
 	return results
 
 def send_statistics_to_server(data):
-	u = urllib2.urlopen("http://xbmc-repo-stats.bimovi.cz/save.php", urllib.urlencode({"data" : simplejson.dumps(data)}), 10)
+	u = urllib2.urlopen("http://xbmc-repo-stats.bimovi.cz/save-stats", urllib.urlencode({"data" : simplejson.dumps(data)}), 10)
 	log("Usage Tracking", [simplejson.dumps(data), u.getcode()])
 	return u.getcode() == 201
 
